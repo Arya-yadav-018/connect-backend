a387e1478272790076ea6be2cb39e4475da1ad5e
wdeveloper.arya@gmail.com <wdeveloper.arya@gmail.com>
2026-06-13 19:50:31 +0530

    fixed error

--- src/services/userService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const backendurl = import.meta.env.VITE_BACKEND_URL;
-
-export const fetchUser = async()=>{
-     return await axios.get(
-          `${backendurl}/api/profile/view`, {withCredentials: true}
-     );
-};
